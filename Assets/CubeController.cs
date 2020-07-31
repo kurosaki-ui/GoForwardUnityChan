e4c21c954b39679e7762d49504a6580cd884e485
kurosaki-ui <p10p030@cis.ac.jp>
2020-08-01 01:52:37 +0900

    初めてのコミット　2020.7.31

---
+++ Assets/CubeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	AudioSource audioData;
+	private float speed = -12;
+	private float deadLine = -10;
+	private float groundLevel = -3.0f;
+	private bool isAudio = true;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(this.speed* Time.deltaTime, 0, 0);
+
+		if(transform.position.x < this.deadLine){
+			Destroy(gameObject);
+		}
+	}
+	 void OnCollisionEnter2D(Collision2D other)
+	{
+		this.audioData = GetComponent<AudioSource>();
+		if(other.gameObject.tag == "CubeTag" || other.gameObject.tag =="GroundTag")
+		{
+			this.audioData.Play(0);
+		}
+    }
+}
